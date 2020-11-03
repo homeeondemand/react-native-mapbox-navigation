ec6af9bb2dd119e49b22e4dbd37c4a58da867a04
Ross Martin <rmartin311@gmail.com>
2020-11-03 11:24:23 -0600

    Remove unused view property

--- ios/MapboxNavigationManager.m
+++ ios/MapboxNavigationManager.m
@@ -6,7 +6,6 @@ RCT_EXPORT_VIEW_PROPERTY(onProgressChange, RCTDirectEventBlock)
 RCT_EXPORT_VIEW_PROPERTY(onError, RCTDirectEventBlock)
 RCT_EXPORT_VIEW_PROPERTY(origin, NSArray)
 RCT_EXPORT_VIEW_PROPERTY(destination, NSArray)
-RCT_EXPORT_VIEW_PROPERTY(isMuted, BOOL)
 RCT_EXPORT_VIEW_PROPERTY(shouldSimulateRoute, BOOL)
 
 @end
